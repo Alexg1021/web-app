1306b68afdca66c2215113c7ede733c65ffd4ef8
Alex Gutierrez <gutierrez.lx@gmail.com>
2016-04-05 09:14:15 -0700

    finished product

--- public/javascripts/app.module.js
+++ public/javascripts/app.module.js
@@ -1,7 +1,19 @@
 (function(){
+  'use strict';
   angular.module('app', ['ui.router', 'app.ui', 'ui.bootstrap'])
     .config(function($stateProvider, $urlRouterProvider){
 
-      
+      /**
+       * Default Route.
+       */
+       $urlRouterProvider.otherwise('/login');
+
+      $stateProvider
+        .state('login', {
+          url: '/login',
+          templateUrl: 'partials/login/index.html',
+          controller: 'LoginController',
+          controllerAs: 'loginControl'
+        });
     });
 })();
